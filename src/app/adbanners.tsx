e77fa9497fda9d09d065ff8c3c819933e8c81a1b
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-31 15:15:00 +0530

    chore: Update next.config.mjs, layout.tsx, package.json, and globals.css, initialised the project

---
+++ src/app/adbanners.tsx
@@ -0,0 +1,72 @@
+export const banners = [
+  {
+      "title": "Satisfy Your Cravings",
+      "description": "Indulge in Delicious Dishes. Contact Us!",
+      "cta": "Order Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/9/square.png",
+      "backgroundTemplate": "bg-yellow-500"
+  },
+  {
+      "title": "Whatever you prefer",
+      "description": "Indulge in the good tech, and find your way!",
+      "cta": "Explore Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/1/square.png",
+      "backgroundTemplate": "bg-gray-800"
+  },
+  {
+      "title": "Do something great",
+      "description": "Bringing you the best of the best from top class market!",
+      "cta": "Order Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/15/square.png",
+      "backgroundTemplate": "bg-yellow-600"
+  },
+  {
+      "title": "Enjoy the Finest",
+      "description": "Delight in our premium selection!",
+      "cta": "Shop Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/12/square.png",
+      "backgroundTemplate": "bg-red-500"
+  },
+  {
+      "title": "Refresh Your Day",
+      "description": "Cool off with our refreshing drinks!",
+      "cta": "Drink Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/9/square.png",
+      "backgroundTemplate": "bg-blue-500"
+  },
+  {
+      "title": "Boost Your Energy",
+      "description": "Grab a healthy snack and get going!",
+      "cta": "Snack Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/15/square.png",
+      "backgroundTemplate": "bg-green-500"
+  },
+  {
+      "title": "Unleash Your Creativity",
+      "description": "Find the best tools for your projects!",
+      "cta": "Create Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/14/square.png",
+      "backgroundTemplate": "bg-purple-500"
+  },
+  {
+      "title": "Discover New Horizons",
+      "description": "Explore new destinations with our travel deals!",
+      "cta": "Travel Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/9/square.png",
+      "backgroundTemplate": "bg-orange-500"
+  },
+  {
+      "title": "Elevate Your Style",
+      "description": "Step up your fashion game with our latest collection!",
+      "cta": "Shop Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/1/square.png",
+      "backgroundTemplate": "bg-pink-500"
+  },
+  {
+      "title": "Tech Savvy",
+      "description": "Get the latest gadgets at unbeatable prices!",
+      "cta": "Buy Now",
+      "image": "https://bannerbot-public.s3.ap-south-1.amazonaws.com/templates/9/square.png",
+      "backgroundTemplate": "bg-indigo-500"
+  }
+]
